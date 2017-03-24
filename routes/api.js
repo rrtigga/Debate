ce10193be9a987d6ec3180b927470dd7f33c50ec
shahzeb1 <shahzeb001@gmail.com>
2017-03-23 21:22:09 -0700

    API changs

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET API listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
