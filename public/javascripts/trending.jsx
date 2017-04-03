302035996aae8d7be7be93a2c0e2f1adeeb79399
rrtigga <rrtigga@ucdavis.edu>
2017-04-03 01:24:30 -0700

    able to repeat with react
    need to work on responsive elements rows

---
+++ public/javascripts/trending.jsx
@@ -0,0 +1,52 @@
+var RepeatModule = React.createClass({
+	getInitialState: function() {
+    	return { items: this.props.items || [] }
+   },
+   componentWillMount: function() {
+     console.log("componentWillMount()")
+     this.setState({ items : data })
+    },
+   render: function() {
+      var listItems = this.state.items.map((item, index) => {
+         return (
+            <ListItem key={index} handleClick={this.handleClick} item={item} />
+         );
+      });
+      return (
+      	<div className="row">
+            {listItems}
+        </div>
+      );
+   }
+});
+/* make the items stateless */
+var ListItem = function(props) {
+    var backgroundImage = {
+      backgroundImage: 'url(' + props.item.imageURL + ')'
+    };
+     return (
+     	<div className="">
+	     	<div className="debate column _33">
+				<div style={backgroundImage} className="debateImage"></div>
+				<p>{props.item.title}</p>
+			</div>
+		</div>
+     );
+}
+var data = [
+	{
+		"title": "Did Trump’s refugee ban make us safer?",
+		"imageURL":"../images/debates/trump.png"
+	},
+	{
+		"title": "Did Trump’s refugee ban make us safer?",
+		"imageURL":"../images/debates/trump.png"
+	},
+	{
+		"title": "Did Trump’s refugee ban make us safer?",
+		"imageURL":"../images/debates/trump.png"
+	}
+];
+ReactDOM.render(<RepeatModule items={data} />, document.getElementById('trending-content'));
+
+
